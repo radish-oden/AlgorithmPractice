ea0d3c6cad61f691ca212659d38fee66d43b28c8
mashiro <toiawase0@gmail.com>
2019-06-03 00:48:53 +0900

    TestCommit

---
+++ UnitTestProject1/BubbleSortTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithm;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BubbleSortTest
+    {
+
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BubbleSort bubbleSort = new BubbleSort();
+            int[] i = bubbleSort.Bubble();
+
+            CollectionAssert.AreEqual(i, numbers);
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ceeca410-b223-4c6f-82b2-fce3d96cd177")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
